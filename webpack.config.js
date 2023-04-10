a6a94f86618823a29c8e81e936016b6a83f32a6d
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2023-04-10 11:32:08 -0300

    chore: configuring style-loader and css-loader with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,18 @@ module.exports = {
         use: "babel-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
