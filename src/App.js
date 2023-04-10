0ceab8a7f8e9a2da2dac456faf49ad9de28e24da
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2023-04-09 21:57:04 -0300

    feat: create App component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1 id="title">
+      <span>Hello World!</span>
+    </h1>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <h1 id="title">
-    <span>Olá mundo!</span>
-  </h1>,
-  document.getElementById("root")
-);
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
